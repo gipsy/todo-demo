e1ca9562314bbaf26f525d16896b1a7d0fb38682
Taras <ter4444@gmail.com>
2018-09-10 13:39:49 +0300

    removed auth

--- src/bundles/index.js
+++ src/bundles/index.js
@@ -1,12 +1,10 @@
 import { composeBundles, createCacheBundle } from 'redux-bundler';
 
-import auth from '@bundles/auth';
 import extraArgs from '@bundles/extra-args';
 import tasks from '@bundles/tasks';
 import cache from '@common/utils/cache';
 
 export default composeBundles(
-  auth,
   tasks,
   createCacheBundle(cache.set),
   extraArgs,
--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -4,32 +4,17 @@ import { hot } from 'react-hot-loader'
 import { connect } from 'redux-bundler-react'
 
 import Home from '@screens/home'
-// import SignIn from '@screens/sign-in'
-// import Private from '@screens/private'
 import TaskListPage from '@screens/task-list'
 
-const AppRoot = ({ signedIn, signedInPending }) => {
-  const requireAuth = (Component) => (signedIn ? Component : SignIn)
-  if (signedInPending) {
-    return null
-  }
+const AppRoot = () => {
   return (
     <Router>
       <>
         <Route path="/" exact component={Home} />
         <Route path="/tasks" component={TaskListPage} />
-        {/* <Route path="/private" component={requireAuth(Private)} /> */}
-        {/* <Route path="/sign-in" component={SignIn} /> */}
-        {/* <Route path="/tasks" component={TaskListPage} /> */}
       </>
     </Router>
   )
 }
 
-export default hot(module)(
-  connect(
-    'selectSignedIn',
-    'selectSignedInPending',
-    AppRoot,
-  ),
-)
+export default hot(module)(AppRoot)
